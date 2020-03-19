830197336fc5e78480f66078d58c0431e9c8b52c
meilixiaozhang <redpangci@gmail.com>
2020-03-19 19:11:08 +1100

    set timer

--- index.js
+++ index.js
@@ -55,4 +55,7 @@ function makeSound(key) {
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("."+currentKey);
   activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },200);
 }
